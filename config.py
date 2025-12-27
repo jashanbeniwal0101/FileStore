4736a9ac6e8f333e43812b78f07f8e4e7aa8b352
jashanbeniwal0101 <beniwaljass733@gmail.com>
2025-12-27 21:00:36 +0530

    Change OWNER_ID from string to integer
    Updated OWNER_ID to be an integer instead of a string.

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from logging.handlers import RotatingFileHandler
 # Bot Configuration
 LOG_FILE_NAME = "bot.log"
 PORT = '5010'
-OWNER_ID = "1955406483"
+OWNER_ID = 1955406483
 
 MSG_EFFECT = 5046509860389126442
 
